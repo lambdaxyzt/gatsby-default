b4d1bfd8989a241055b796d8ac8b75115a49c748
Lambdaxyzt <lambdaxyzt@gmail.com>
2022-01-17 02:46:35 -0800

    some change for ImageC component

--- src/components/atom/ImageC/index.js
+++ src/components/atom/ImageC/index.js
@@ -1,8 +1,6 @@
 import * as React from "react"
-import * as styles from "./index.module.scss"
 import {graphql, useStaticQuery} from "gatsby";
 import {GatsbyImage} from "gatsby-plugin-image";
-
 const Index = (props) => {
     const query = useStaticQuery(graphql`
         query Icons {
@@ -19,26 +17,19 @@ const Index = (props) => {
         }
     `)
     const icons = query.allFile.nodes
-    const icon = icons.filter(item => item.name == (props.imageName || "Frontpage-SBTi"))[0]
-    const extension = icon.extension
-    if (extension == 'svg') {
-        return (
-            <img
-                className={styles.image}
-                src={icon.publicURL}
-                {...props}
-            />
-        )
-    } else {
-        return (
-            <GatsbyImage
-                objectFit="JJ"
-                className={styles.image}
-                image={icon.childImageSharp.gatsbyImageData}
-                {...props}
+    const icon = icons.filter(item => item.name == (props.imageName || "gatsby-icon"))[0]
+    return (
+        <GatsbyImage
+            image={icon.childImageSharp.gatsbyImageData}
+            style={{
+                height:"100%",
+                width:"100%",
+            }}
+            imgStyle={{
+                objectFit:"cover"
+            }}
 
-            />
-        )
-    }
+        />
+    )
 }
 export default Index;
