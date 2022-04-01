985b4deaa3bc5023264bf58b81e68a562fc7faae
Lambdaxyzt <lambdaxyzt@gmail.com>
2022-03-31 19:18:21 -0700

    update yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,62 +1,72 @@
 const path = require('path');
+require('dotenv').config()
+
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `Lambdaxyzt`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-react-svg`,
-    `gatsby-plugin-image`,
-    `gatsby-transformer-json`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-        resolve: "gatsby-plugin-root-import",
-        options: {
-            resolveModules: [path.join(__dirname, "src")],
+    siteMetadata: {
+        title: ``,
+        description: ``,
+        author: ``,
+        siteUrl: ``,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-react-svg`,
+        `gatsby-plugin-image`,
+        `gatsby-transformer-json`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        // *** Root Import ***
+        {
+            resolve: "gatsby-plugin-root-import",
+            options: {
+                resolveModules: [path.join(__dirname, "src")],
+            },
         },
-    },
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `image`,
-            path: `${__dirname}/src/asset/image`,
+        // *** source file system ***
+        // image :
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `image`,
+                path: path.resolve(process.env.IMAGE_FOLDER),
+            },
         },
-    },
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `data`,
-            path: `${__dirname}/src/data`,
+        // data :
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data`,
+            },
         },
-    },
-    {
-      resolve: `gatsby-plugin-styled-jsx`,
-      options: {
-          jsxPlugins:
-              ["@styled-jsx/plugin-sass"],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-default`,
-        short_name: `default`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/asset/image/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+        // *** styling options *** :
+        // styled-jsx
+        {
+            resolve: `gatsby-plugin-styled-jsx`,
+            options: {
+                jsxPlugins:
+                    ["@styled-jsx/plugin-sass"],
+            },
+        },
+        // emotion
+        `gatsby-plugin-sass`,
+        // *** manifest plugin ***
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-default`,
+                short_name: `default`,
+                start_url: `/`,
+                background_color: `#663399`,
+                // This will impact how browsers show your PWA/website
+                // https://css-tricks.com/meta-theme-color-and-trickery/
+                // theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/asset/image/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
