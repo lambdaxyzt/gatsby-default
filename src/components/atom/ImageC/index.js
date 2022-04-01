49c714c5e468f0b1359c4971feddbf85354e3ceb
Lambdaxyzt <lambdaxyzt@gmail.com>
2022-03-31 19:40:39 -0700

    add Alink component

--- src/components/atom/ImageC/index.js
+++ src/components/atom/ImageC/index.js
@@ -1,72 +1,75 @@
 import * as React from "react"
-import {graphql, useStaticQuery} from "gatsby";
-import {GatsbyImage} from "gatsby-plugin-image";
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 const Index = (props) => {
     const query = useStaticQuery(graphql`
-        query Icons {
-            allFile{
+        query Images {
+            allFile(filter: {sourceInstanceName: {eq: "image"}}) {
                 nodes {
                     name
-                    publicURL
                     extension
                     childImageSharp {
-                        gatsbyImageData
+                        gatsbyImageData (
+                            quality: 90
+                            formats:[JPG]
+                            backgroundColor: "white"
+                            breakpoints: [1920]
+                        )
                     }
                 }
             }
         }
+
     `)
-    React.useEffect(() => {
-        const html = document.querySelector('html')
-        const body = document.querySelector('body')
-    })
     const icons = query.allFile.nodes
-    let icon = icons.filter(item => item.name == (props.imageName || "default"))[0];
-    const default_ = icons.filter(item => item.name == "default")[0];
+    let icon = icons.filter(item => item.name == (props.imageName || "default"))[0]
+    const default_ = icons.filter(item => item.name == "default")[0]
     if (!icon) {
-        icon = default_;
+        icon = default_
     }
     if (props.type == "background") {
-        console.log(icon.publicURL)
         return (
             <>
                 <style jsx>{`
-                  .root {
-                    background: url(${icon.publicURL});
-                  }
-                `}</style>
+          .root {
+            background: url(${icon.childImageSharp.gatsbyImageData.images.fallback.src});
+          }
+        `}</style>
 
-                <div className="root"
-                     style={{
-                         height: props.height ? props.height : "100%",
-                         width: props.width ? props.width : "100%",
-                         backgroundRepeat: "no-repeat",
-                         backgroundAttachment: "fixed",
-                         backgroundSize: "cover",
-                     }}
+                <div
+                    className={"root " + props.className}
+                    style={{
+                        height: props.height ? props.height : "100%",
+                        width: props.width ? props.width : "100%",
+                        backgroundRepeat: "no-repeat",
+                        backgroundAttachment: "fixed",
+                        backgroundSize: "contain",
+                        ...props.style
+                    }}
                 >
-
                 </div>
             </>
         )
     }
     return (
         <GatsbyImage
+            className={props.className}
             image={icon.childImageSharp.gatsbyImageData}
             style={{
                 height: props.height ? props.height : "100%",
                 width: props.width ? props.width : "100%",
-
+                ...props.style
             }}
             imgStyle={{
                 objectFit: props.objectFit ? props.objectFit : "cover",
                 borderRadius: props.borderRadius ? props.borderRadius : null,
                 overflow: "hidden",
+                objectPosition: props.objectPosition ? props.objectPosition : "",
             }}
             alt={props.alt}
 
         />
     )
 }
-export default Index;
+export default Index
